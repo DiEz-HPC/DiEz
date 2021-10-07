ee24c4780d7ef46dcfd023ec6eee742d2b108677
bouboumael <bouboumael@orange.fr>
2021-10-07 14:41:56 +0200

    create pages and defined css

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,20 @@
 import React from "react";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from "./views/pages/Home";
+import About from "./views/pages/About";
+import Blog from "./views/pages/Blog";
+import Contact from "./views/pages/Contact";
+import './styles/app.scss';
 
 
 export default function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path='/' component={Home}/>
+                <Route exact path={'/'} component={Home}/>
+                <Route exact path={'/qui-sommes-nous'} component={About} />
+                <Route exact path={'/actualites'} component={Blog} />
+                <Route exact path={'/contact'} component={Contact} />
             </Switch>
         </BrowserRouter>
     );
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './styles/app.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
