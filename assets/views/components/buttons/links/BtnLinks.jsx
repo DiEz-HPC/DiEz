6120383f35733f46a083a5612c4b7bc99ba9cd97
bouboumael <bouboumael@orange.fr>
2021-10-08 18:38:10 +0200

    faker

---
+++ assets/views/components/buttons/links/BtnLinks.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './btnlinks.scss'
+
+function BtnLinks(props) {
+    const {link, label} = props
+    return (
+        <div className={'btnLink'}>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                <defs>
+                    <filter id="gooey">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur"/>
+                        <feColorMatrix in="blur" type="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                                       result="highContrastGraphic"/>
+                        <feComposite in="SourceGraphic" in2="highContrastGraphic" operator="atop"/>
+                    </filter>
+                </defs>
+            </svg>
+            <a href={link} id="gooey-button">
+                {label}
+                <span className="bubbles">
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                    <span className="bubble"/>
+                </span>
+            </a>
+        </div>
+    );
+}
+
+BtnLinks.propTypes = {
+    link: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    style: PropTypes.string.isRequired,
+}
+
+export default BtnLinks
--- assets/views/pages/Home.jsx
+++ assets/views/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import '../../styles/home.scss'
+import BtnLinks from "../components/buttons/links/BtnLinks";
 
 function Home() {
     return (
         <div className={'home'}>
             <h1>DiEz <br/>Dev It Easy</h1>
+            <BtnLinks link={'/'} label={'Accueil'} style={'btn btn-danger'} />
         </div>
     )
 }
