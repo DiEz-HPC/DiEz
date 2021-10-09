a8d598e4957cdc924edea9a7070116cd5603d3b2
bouboumael <bouboumael@gmail.com>
2021-10-09 11:39:36 +0200

    color and size adjust element

--- assets/views/components/buttons/links/BtnLinks.jsx
+++ assets/views/components/buttons/links/BtnLinks.jsx
@@ -3,20 +3,20 @@ import PropTypes from "prop-types";
 import './btnlinks.scss'
 
 function BtnLinks(props) {
-    const {link, label} = props
+    const {link, label, style} = props
     return (
         <div className={'btnLink'}>
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                 <defs>
                     <filter id="gooey">
-                        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur"/>
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur"/>
                         <feColorMatrix in="blur" type="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
                                        result="highContrastGraphic"/>
                         <feComposite in="SourceGraphic" in2="highContrastGraphic" operator="atop"/>
                     </filter>
                 </defs>
             </svg>
-            <a href={link} id="gooey-button">
+            <a href={link} id="link-button">
                 {label}
                 <span className="bubbles">
                     <span className="bubble"/>
