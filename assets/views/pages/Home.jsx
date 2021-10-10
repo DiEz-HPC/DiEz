0782219b709a38f371ad607bbb8b8464418ee9e7
bouboumael <bouboumael@gmail.com>
2021-10-10 11:13:43 +0200

    create all btn

--- assets/views/pages/Home.jsx
+++ assets/views/pages/Home.jsx
@@ -6,13 +6,6 @@ function Home() {
     return (
         <div className={'home'}>
             <h1>DiEz <br/>Dev It Easy</h1>
-            <div className="d-flex">
-                <BtnLinks link={'/'} label={'View our work'} color={'primary'} variant={'outlined'} />
-                <BtnLinks link={'/'} label={'Contact Us'} color={'dark'} variant={'contained'} />
-                <BtnLinks link={'/'} label={'Get in touch'} color={'primary'} variant={'contained'} />
-                <BtnLinks link={'/'} label={'View Project'} color={'white'} variant={'outlined'} />
-                <BtnLinks link={'/'} label={'View all reviews'} color={'dark'} variant={'outlined'} />
-            </div>
         </div>
     )
 }
