9d952a2ae31d65c156f58d2b0841a00ed9f23812
RedPandore <tennessee-houry_student2021@wilder.school>
2021-11-04 15:26:30 +0100

    Finish staff part

---
+++ assets/views/components/About/First-Part/FirstPart.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Dot from '../../dot/Dot';
+import './FirstPart.scss';
+
+function FirstPart() {
+    return (
+        <div className="first-part">
+            <div className="container">
+                <div className="row d-flex justify-content-center">
+                    <h2 className="text-start">
+                        À propos de DevItEasy
+                        <Dot />
+                    </h2>
+                    <div className="col-md-6">
+                        <p>
+                            DevItEasy est une plateforme de développement web et
+                            mobile. Elle permet de créer des sites web et des
+                            applications mobiles.
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Architecto obcaecati itaque, dolores atque
+                            similique nesciunt nemo tempore modi sequi voluptas
+                            eaque deserunt pariatur quod culpa voluptatibus
+                            molestiae? Nemo, delectus nobis!
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Architecto obcaecati itaque, dolores atque
+                            similique nesciunt nemo tempore modi sequi voluptas
+                            eaque deserunt pariatur quod culpa voluptatibus
+                            molestiae? Nemo, delectus nobis!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default FirstPart;
---
+++ assets/views/components/About/Staff/Personal/Personal.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Personal.scss';
+
+function Personal(props) {
+    const { index, render } = props;
+    return (
+        <>
+            <div className={`text-box ${index === 0 ? 'text-on' : 'text-off'}`}>
+                <div className="d-flex flex-column-reverse h-100">
+                    <div className="d-flex justify-content-between">
+                        <div className="d-flex flex-column-reverse mb-2 staff-info">
+                            <p className="px-3 m-0 staff-status">{props.Status}</p>
+                            <p className="px-3 m-0 staff-name">
+                                {props.firstName} {props.lastName}
+                            </p>
+                        </div>
+                        <div className="mb-2 staff-social">
+                            <ul className="m-0 list-unstyled px-4">
+                                <li>
+                                    <a href={props.linkedin} target="_blank">
+                                        <i className="fab fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href={`mailto:${props.email}`}
+                                        target="_blank"
+                                    >
+                                        <i className="fas fa-envelope-square"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                className="img-box"
+                onMouseEnter={(e) => {
+                    render(index);
+                }}
+            >
+                <img className="Imagetest" src={props.image} alt="" />
+            </div>
+        </>
+    );
+}
+
+export default Personal;
