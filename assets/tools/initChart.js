8785377a3110d12f4ab99e6a799be4491f471da4
RedPandore <tennessee-houry_student2021@wilder.school>
2021-11-05 14:19:25 +0100

    import library

--- assets/tools/initChart.js
+++ assets/tools/initChart.js
@@ -1,8 +1,8 @@
+import '@symfony/ux-chartjs/src/controller';
 const graphs = [...document.getElementsByTagName('canvas')];
 
 graphs.map( async (graph) => {
     const data = await JSON.parse(graph.dataset.view);
-    console.log(data)
     const myChart = new Chart(graph, data);
 })
 
