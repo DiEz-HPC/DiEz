d54ab9293d5ec97f15c58e2ba23b01b3f0df5efc
RedPandore <tennessee-houry_student2021@wilder.school>
2021-11-11 16:38:51 +0100

    refactor Js and fix css placement

--- assets/tools/changeColorAccordingBackgroundColor.js
+++ assets/tools/changeColorAccordingBackgroundColor.js
@@ -1,62 +1,59 @@
-function changeColorAccordingBackgroundColor(body, elementChange, colorsForChange = {}, notElements = []) {
+function changeColorAccordingBackgroundColor(
+    elementChange,
+    body,
+    colorsForChange = {},
+    notElements = []
+) {
     let color = 'rgb(255, 255, 255)';
-    const allElements = [...document.getElementById(body).children[0].children];
+    const allElements = [...document.getElementById(body).querySelectorAll('*')];
     const positionElement = elementChange.getBoundingClientRect();
     let colors = [];
     allElements
-        .filter(element =>
-            element !== elementChange &&
-            !notElements.includes(elementChange) &&
-            positionElement.top >= element.getBoundingClientRect().top &&
-            positionElement.top <= (element.getBoundingClientRect().top + element.getBoundingClientRect().height)
+        .filter(
+            (element) =>
+                element !== elementChange &&
+                !notElements.includes(elementChange) &&
+                positionElement.top >= element.getBoundingClientRect().top &&
+                positionElement.top <=element.getBoundingClientRect().top + element.getBoundingClientRect().height &&
+                positionElement.width >=element.getBoundingClientRect().left &&
+                positionElement.width <=element.getBoundingClientRect().left + element.getBoundingClientRect().width
         )
-        .map(element => {
-            const selectors = [...element.querySelectorAll('*')];
+        .map((element) => {
             colors.push(window.getComputedStyle(element).backgroundColor);
-            selectors
-                .filter(selector =>
-                    positionElement.top >= selector.getBoundingClientRect().top &&
-                    positionElement.top <= (selector.getBoundingClientRect().top + selector.getBoundingClientRect().height) &&
-                    positionElement.width >= selector.getBoundingClientRect().left &&
-                    positionElement.width <= (selector.getBoundingClientRect().left + selector.getBoundingClientRect().width)
-                )
-                .map(selector => {
-                    colors.push(window.getComputedStyle(selector).backgroundColor);
-
-                    console.log(positionElement.left, element.getBoundingClientRect().left, selector.getBoundingClientRect().left);
-                })
-
             for (let i = colors.length - 1; i >= 0; i--) {
-                if (colors[i] !== 'rgba(0, 0, 0, 0)' && colors[i] !== 'rgb(255, 255, 255)') {
+                if (
+                    colors[i] !== 'rgba(0, 0, 0, 0)' &&
+                    colors[i] !== 'rgb(255, 255, 255)'
+                ) {
                     color = colors[i];
                     break;
                 }
             }
-        })
-    color = color === 'rgb(255, 255, 255)' ? colorsForChange.light : colorsForChange.dark;
+        });
+    color = color === 'rgb(255, 255, 255)' ? colorsForChange.dark : colorsForChange.light;
     return color;
 }
 
+function initChangeColor(
+    element,
+    parent = 'root',
+    dark = 'rgb(0, 0, 0)',
+    light = 'rgb(255, 255, 255)',
+    transition = '0.2s'
+) {
+    const elements = [...element.querySelectorAll('*')];
+    elements.map((element) => {
+        const color = changeColorAccordingBackgroundColor(element, parent, {
+            dark: dark,
+            light: light,
+        });
+        element.style.transition = 'color ' + transition;
+        element.style.color = color;
+    });
+}
 
 window.onload = () => {
     window.onscroll = () => {
-        const elements = [...document.getElementById('social').children];
-        elements.map(element => {
-            const color = changeColorAccordingBackgroundColor(
-                'root',
-                element,
-                {
-                    dark: 'rgb(0, 0, 0)',
-                    light: '#56C6FF'
-                },
-            );
-
-            if (element.tagName === 'A') {
-                element.style.setProperty('background-color', color);
-            }
-            element.style.transition = 'color .4s'
-            element.style.color = color;
-
-        })
+        initChangeColor(document.getElementById('social'));
     };
 };
--- assets/views/components/Social/Social.jsx
+++ assets/views/components/Social/Social.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import LineIcon from "react-lineicons";
-import '../Social/social.scss'
+import '../Social/social.scss';
 
 function Social() {
+    const [text, setText] = React.useState([...'nous suivre']);
     return (
         <div id={'social'} className="social">
-            <p className="text-social m-auto">NOUS SUIVRE</p>
+            <p className="text-social">
+                {text.map((letter, index) => {
+                    return <span key={index}>{letter}</span>;
+                }
+                )}
+            </p>
+
             <hr className="hr-social"/>
-            <a href="#" className=" m-auto">
-                <LineIcon name="facebook"/>
+            <a href="#" className="ms-3">
+                <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="#" className=" m-auto">
-                <LineIcon name="twitter"/>
+            <a href="#" className="ms-4">
+                <i className="fab fa-twitter"></i>
             </a>
-            <a href="#" className=" m-auto">
-                <LineIcon name="instagram"/>
+            <a href="#" className="ms-4">
+                <i className="fab fa-instagram"></i>
             </a>
-            <a href="#" className=" m-auto">
-                <LineIcon name="linkedin"/>
+            <a href="#" className="ms-4">
+                <i className="fab fa-linkedin-in"></i>
             </a>
+
         </div>
     );
 }
