f5c1e33c05a23a30ac6ff8ce1fc2f58a31595d06
Loic-Code <loic.pinguet.code@gmail.com>
2021-11-15 10:48:49 +0100

    contact first part

--- assets/views/pages/Contact.jsx
+++ assets/views/pages/Contact.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import "../../styles/contact.scss";
+import wave from "../../images/wave.svg";
+import Navbar from "../components/navbar/Navbar";
+import Social from "../components/Social/Social";
+import FeedLine from "../components/feed/FeedLine";
+import Dot from "../components/dot/Dot";
+import SecondPart from "../components/Contact/SecondPart/SecondPart";
+import FirstPart from "../components/Contact/FirstPart/FirstPart";
 
 function Contact() {
-    return (
-        <div className={'contact'}>
-            <h1>Contactez nous</h1>
+  return (
+    <div className={"contact"}>
+      <div className="header">
+        <img id={"wave"} className={"wave"} src={wave} alt="wave" />
+        <Navbar />
+        <div className="feedLineBox">
+          <FeedLine title="contact" />
+          <h2 className="pageTitle mt-2">
+            Contact
+            <Dot />
+          </h2>
         </div>
-    )
+      </div>
+      <Social />
+      <section className={"contact_first_part"}>
+        <FirstPart />
+      </section>
+      <section className="contact_second_part">
+        <SecondPart />
+      </section>
+    </div>
+  );
 }
 
 export default Contact;
---
+++ src/Controller/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+#[Route('/api/v2', name: 'contact_')]
+class ContactController extends AbstractController
+{
+    private Serializer $serializer;
+
+    public function __construct()
+    {
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $this->serializer = new Serializer($normalizers, $encoders);
+    }
+
+    #[Route('/contact', name: 'form')]
+    public function index(Request $request): Response
+    {
+        $userMessage =  $this->serializer->deserialize($request->getContent(), ContactMessage::class, 'json');
+       dd($userMessage);
+
+    }
+}
