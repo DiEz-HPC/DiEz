58dd41d30c69cece385352e88914f1001e1ecde1
RedPandore <tennessee-houry_student2021@wilder.school>
2021-11-16 10:15:42 +0100

    Fixture vistor

--- src/DataFixtures/VisitorCounterFixutres.php
+++ src/DataFixtures/VisitorCounterFixutres.php
@@ -2,16 +2,28 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTimeZone;
+use DateTimeImmutable;
+use App\Entity\VisitorCounter;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class VisitorCounterFixutres extends Fixture
 {
+    private const NB_MONTH = 12;
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $year = date('Y');
+        for ($month = 1; $month <= self::NB_MONTH; $month++) {
+            for ($day = 1; $day <= rand(0, 30); $day++)
+            {
+                $visitorCounter = new VisitorCounter();
+                $visitorCounter->setDate(new DateTimeImmutable("$year-$month-$day", new DateTimeZone('Europe/Paris')));
+                $visitorCounter->setIpAdress(rand(0, 255) . '.' . rand(0, 255) . '.' . rand(0, 255) . '.' . rand(0, 255));
+                $manager->persist($visitorCounter);
+            }
+        }
         $manager->flush();
     }
 }
