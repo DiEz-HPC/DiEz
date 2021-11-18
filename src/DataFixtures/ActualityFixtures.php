b74815a9bde3e556d10b516aaca1348b1aab4dd6
RedPandore <tennessee-houry_student2021@wilder.school>
2021-11-18 11:59:32 +0100

    actuality fixture

---
+++ src/DataFixtures/ActualityFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ActualityFixtures extends Fixture
+{
+    const AUTHOR = [
+        'Tens',
+        'Mael',
+        'Pings'
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 10; $i++) {
+            $actuality = new Post();
+            $actuality->setTitle($faker->sentence(3));
+            $actuality->setSlug($faker->slug());
+            $actuality->setUrl($faker->url());
+            $actuality->setCreatedAt(new \DateTimeImmutable('now'));
+            $actuality->setAuthor(array_rand(self::AUTHOR));
+            $actuality->setArticle($faker->paragraph(4, true));
+            $manager->persist($actuality);
+        }
+        $manager->flush();
+    }
+}
