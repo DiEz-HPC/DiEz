f0242bef7a2e216b1d3a3b4d71a84b66d41238ad
bouboumael <bouboumael@gmail.com>
2021-11-25 12:33:03 +0100

    deploy.sh

--- symfony-app/deploy.sh
+++ symfony-app/deploy.sh
@@ -1,6 +1,6 @@
-composer install -y
-apt update && apt upgrade && apt install npm && npm install -y
-npm run build -y
+composer install
+apt update && apt upgrade && apt install npm && npm install
+npm run build
 php bin/console assets:install public
 php bin/console cache:clear
 php bin/console cache:warmup
