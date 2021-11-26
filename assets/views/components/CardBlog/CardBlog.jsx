447991e60c02419d4aa2799f31551b0b61416feb
Loic-Code <loic.pinguet.code@gmail.com>
2021-11-26 12:08:34 +0100

    fix blog

--- assets/views/components/CardBlog/CardBlog.jsx
+++ assets/views/components/CardBlog/CardBlog.jsx
@@ -7,11 +7,12 @@ import {timeToRead} from "../../../tools/timeToRead";
 function CardBlog(props) {
   const { article, title, imageName, author, slug, createdAt } = props;
 
+  const name = author.split(" ");
+  const firstName = name.length === 1 ? name[0] : name[1];
+
   const fullArticle = article.split(" ");
   const articlePreview = fullArticle.slice(0, 20);
   const articlePreviewString = articlePreview.join(" ") + " [...]";
-
-  console.log(articlePreviewString);
 
   const day = new Date(createdAt?.timestamp * 1000).toLocaleDateString(
     "fr-FR",
@@ -49,7 +50,7 @@ function CardBlog(props) {
             <div className="day">{day}</div>
             <div className="month">{month}</div>
           </div>
-          <img className="w-100 h-75" src={imageUploadUrl(imageName)} />
+          <img className="w-100 h-100" src={imageUploadUrl(imageName)} />
         </div>
 
         <div className="post-content d-flex flex-column justify-content-between col-12">
@@ -63,8 +64,8 @@ function CardBlog(props) {
           </div>
           <div className="post-meta d-flex flex-column flex-xl-row justify-content-between col-12">
             <div className="user d-flex align-items-center col-12 col-md-3 mb-5 mb-xl-0">
-              <i className="fa fa-user icon-user me-2"></i>
-              <span>{author}</span>
+              <i className="fa fa-user icon-user me-4"></i>
+              <span className="row text-truncate">{firstName}</span>
             </div>
             <div className="d-flex justify-content-center align-items-center col-12 col-xl-6 me-xl-3">
               <Button
