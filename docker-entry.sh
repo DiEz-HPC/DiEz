18a70947ab5bf13828eca67709674d475412bc71
tennessee <houry9@gmail.com>
2021-11-26 12:12:44 +0100

    test3

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-## Symfony cconfiguration
-php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
-php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
+composer install
+apt update -y && apt upgrade -y && apt install npm -y && npm install -y
+npm run build
 
-php bin/console cache:clear
-php bin/console cache:warmup
-
+## Symfony configuration
 chmod -R 777 /var/www/var
 chmod -R 777 /var/www/public
 
-## server config
-php-fpm &
-nginx -g "daemon off;"
+php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
+php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
+
+php bin/console assets:install public
+php bin/console cache:clear
+php bin/console cache:warmup
