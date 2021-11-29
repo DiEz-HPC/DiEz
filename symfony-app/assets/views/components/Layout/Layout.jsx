8b712ca7779e21fecfe6e5e671c4a52c2dec9746
bouboumael <bouboumael@gmail.com>
2021-11-29 16:40:01 +0100

    social entity, api, components

--- symfony-app/assets/views/components/Layout/Layout.jsx
+++ symfony-app/assets/views/components/Layout/Layout.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Social from '../Social/Social'
 import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
 import Cursor from '../Cursor/Cursor'
 import ScrollUp from '../ScrollUp/ScrollUp'
+import {getAllSocials} from "../../../queries/socials";
 
 export default function Layout({ children }) {
+
+    const  [socials, setSocials] = useState([]);
+    useEffect(async () => {
+        const query = await getAllSocials;
+        const data = await query.json();
+        setSocials(data);
+    }, []);
+
   return (
     <div>
       <Cursor />
-      <Navbar />
-      <Social />
+      <Navbar socials={socials} />
+      <Social socials={socials} />
       <ScrollUp />
       <div>{children}</div>
       <Footer />
