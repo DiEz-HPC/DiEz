6f767cda55d5f86eac860fdad733bf84abf6357f
tennessee <houry9@gmail.com>
2022-03-14 12:56:29 +0100

    matomo init

--- symfony-app/assets/app.js
+++ symfony-app/assets/app.js
@@ -17,7 +17,19 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 AOS.init();
-
+/* matomo */
+var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.caprover.deviteasy.fr/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+  /* matomo */
 export default function App() {
     return (
         <BrowserRouter>
