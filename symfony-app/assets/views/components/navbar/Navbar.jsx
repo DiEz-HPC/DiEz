9be67d5600da60807fb7506110e4d18627810993
RedPandore <houry9@gmail.com>
2023-02-13 15:41:35 +0100

    remove link from navbar

--- symfony-app/assets/views/components/navbar/Navbar.jsx
+++ symfony-app/assets/views/components/navbar/Navbar.jsx
@@ -51,9 +51,6 @@ function Navbar(props) {
                             <a href="/qui-sommes-nous">Qui sommes-nous ?</a>
                         </li>
                         <li>
-                            <a href="/nos-prestations">Nos préstations</a>
-                        </li>
-                        <li>
                             <a href="/blog">Blog</a>
                         </li>
 
