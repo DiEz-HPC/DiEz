d29fe685ae1fbd6ab210c95426c123c7f8c02038
RedPandore <houry9@gmail.com>
2023-02-21 11:35:30 +0100

    fix-footer

--- symfony-app/assets/views/components/Prestations/Prestations.jsx
+++ symfony-app/assets/views/components/Prestations/Prestations.jsx
@@ -2,32 +2,32 @@ import React, { useEffect, useState } from 'react';
 import PrestationsCard from './PrestationsCard';
 import './prestation.scss';
 import wave from '../../../images/wave.svg';
-import { getAllPrestation } from '../../../queries/prestations';
-function Prestations() {
+
+function Prestations(props) {
     const [prestations, setPrestations] = useState([]);
     useEffect(() => {
-        getAllPrestation.then((response) => {
-            response.json().then((data) => {
-                setPrestations(data);
-            });
-        });
-    }, []);
+        setPrestations(props.prestations);
+    }, [props.prestations]);
 
     return (
         <section className={'prestation-section row'}>
             <div className={'waveBox'}>
                 <img id={'wave'} className={'wave'} src={wave} alt="wave" />
             </div>
-            <div className={'prestationCardContainer row justify-content-around align-items-center'}>
-            {prestations.map((prestation) => (
-                <PrestationsCard
-                    icon={prestation.icon}
-                    title={prestation.title}
-                    intro={prestation.intro}
-                    link={'/prestations/' + prestation.id}
-                    key={prestation.id}
-                />
-            ))}
+            <div
+                className={
+                    'prestationCardContainer row justify-content-around align-items-center'
+                }
+            >
+                {prestations.map((prestation) => (
+                    <PrestationsCard
+                        icon={prestation.icon}
+                        title={prestation.title}
+                        intro={prestation.intro}
+                        link={'/prestations/' + prestation.id}
+                        key={prestation.id}
+                    />
+                ))}
             </div>
         </section>
     );
--- symfony-app/assets/views/pages/Home.jsx
+++ symfony-app/assets/views/pages/Home.jsx
@@ -1,52 +1,69 @@
-import React, { Suspense } from 'react'
-import '../../styles/home.scss'
-import pcFirst from '../../images/pc_first.webp'
-import Layout from '../components/Layout/Layout'
-import Prestations from '../components/Prestations/Prestations'
+import React, { Suspense, useState, useEffect } from 'react';
+import '../../styles/home.scss';
+import pcFirst from '../../images/pc_first.webp';
+import Layout from '../components/Layout/Layout';
+import Prestations from '../components/Prestations/Prestations';
+import { getAllPrestation } from '../../queries/prestations';
 
 function Home() {
-  const Description = React.lazy(() =>
-    import('../components/description/Description'),
-  )
-  const CarouselProjects = React.lazy(() =>
-    import('../components/carousels/carouselProjects/CarouselProjects'),
-  )
-  const HomeBlog = React.lazy(() => import('../components/HomeBlog/HomeBlog'))
-  const WhatWeDo = React.lazy(() => import('../components/WhatWeDo/Whatwedo'))
-  return (
-    <Suspense fallback={<div>Chargement...</div>}>
-      <Layout>
-        <div className={'home'}>
-          <section className="first-view background-blue d-flex flex-column justify-content-between">
-            <div className={'image-pc'}>
-              <img className={'pc'} src={pcFirst} alt="pc en arrière plan" />
-            </div>
-            <Description />
-            <div className={'arrowBox text-center mb-3'}>
-              <a href="#projects">
-                {' '}
-                <i
-                  className="fas fa-angle-double-down arrow"
-                  id={'arrowIcon'}
-                />{' '}
-              </a>
-            </div>
-          </section>
-  
-          <Prestations />
-          <section className={'carousel-projects'}>
-            <CarouselProjects />
-          </section>
-          <section className={'HomeBlog mt-md-5 pt-md-5'}>
-            <HomeBlog />
-          </section>
-          <section className={'what-we-do mt-md-5 pt-md-5'}>
-            <WhatWeDo />
-          </section>
-        </div>
-      </Layout>
-    </Suspense>
-  )
+    const [prestations, setPrestations] = useState([]);
+    useEffect(() => {
+        getAllPrestation.then((response) => {
+            response.json().then((data) => {
+                setPrestations(data);
+            });
+        });
+    }, []);
+    const Description = React.lazy(() =>
+        import('../components/description/Description')
+    );
+    const CarouselProjects = React.lazy(() =>
+        import('../components/carousels/carouselProjects/CarouselProjects')
+    );
+    const HomeBlog = React.lazy(() =>
+        import('../components/HomeBlog/HomeBlog')
+    );
+    const WhatWeDo = React.lazy(() =>
+        import('../components/WhatWeDo/Whatwedo')
+    );
+    return (
+        <Suspense fallback={<div>Chargement...</div>}>
+            <Layout prestations={prestations}>
+                <div className={'home'}>
+                    <section className="first-view background-blue d-flex flex-column justify-content-between">
+                        <div className={'image-pc'}>
+                            <img
+                                className={'pc'}
+                                src={pcFirst}
+                                alt="pc en arrière plan"
+                            />
+                        </div>
+                        <Description />
+                        <div className={'arrowBox text-center mb-3'}>
+                            <a href="#projects">
+                                {' '}
+                                <i
+                                    className="fas fa-angle-double-down arrow"
+                                    id={'arrowIcon'}
+                                />{' '}
+                            </a>
+                        </div>
+                    </section>
+
+                    <Prestations prestations={prestations} />
+                    <section className={'carousel-projects'}>
+                        <CarouselProjects />
+                    </section>
+                    <section className={'HomeBlog mt-md-5 pt-md-5'}>
+                        <HomeBlog />
+                    </section>
+                    <section className={'what-we-do mt-md-5 pt-md-5'}>
+                        <WhatWeDo />
+                    </section>
+                </div>
+            </Layout>
+        </Suspense>
+    );
 }
 
-export default Home
+export default Home;
